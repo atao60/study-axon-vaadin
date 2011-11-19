702a36aa0ac177a82268ca79c4b5a548d9c79922
merdogan <mstf.erdogan@gmail.com>
2011-11-19 08:10:11 +0200

    Test.

--- oms-core/src/main/java/com/acme/oms/Runner.java
+++ oms-core/src/main/java/com/acme/oms/Runner.java
@@ -42,6 +42,8 @@ public class Runner {
                                              order.getProductId(),
                                              order.getStatus()));
         }
+        
+        
     }
 
 }
